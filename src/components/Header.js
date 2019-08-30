9814a6a445fc53dce0552f550b54c693c3445548
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-08-29 22:02:37 -0400

    Header component added

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+    render(){
+        return(
+            <div className="Header" style={{"height": 100}}>
+                <h1>Clicky Game</h1>
+                <h2>Click on image</h2>
+            </div>
+        )
+    }
+}
+
+export default Header;
+
+
