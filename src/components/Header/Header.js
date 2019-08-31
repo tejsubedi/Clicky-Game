c20d648fc1844779c2ae9a380067bc00ad04da9c
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-08-31 18:01:41 -0400

    header height update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
 
     render(){
         return(
-            <div className="Header" style={{"height": 100}}>
+            <div className="Header" style={{"height": 110}}>
                 <h1>Clicky Game !</h1>
                 <h5>Click on an image to earn points, but don't click on any more than once!</h5>
             </div>
