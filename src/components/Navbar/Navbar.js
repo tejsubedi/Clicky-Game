275e991ee7564c8cc4eae458b4ad3360389b13fc
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-08-31 18:06:40 -0400

    clean the code

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,29 +8,12 @@ const Navbar = props => (
                         <a href="/">Clicky Game</a>
                     </li>
                     <li>Click an image to begin!</li>
-                    <li className= "score">Your Score {props.total}</li>
-                    <li className="totalScore">Total Score {props.total}</li>
+                    <li className= "score">Your Score:  {props.total}</li>
+                    <li className="totalScore">Total Score:  {props.total}</li>
                     <li className = "status">{props.status}</li>
                 </ul>
     </nav>
 );
 
-// class Navbar extends Component {
-
-//     render() {
-//         return (
-//             <nav className="navbar">
-//                 <ul>
-//                     <li className="brand">
-//                         <a href="/">Clicky Game</a>
-//                     </li>
-//                     <li>Click an image to begin!</li>
-//                     <li>Score:0</li>
-//                     <li>Top Score:0</li>
-//                 </ul>
-//             </nav>
-//         )
-//     }
-// }
 
 export default Navbar;
